98c8476c00e402771d71283eb1adb510aa6dbaae
Mohit Badola <94218660+mohitbadola@users.noreply.github.com>
2025-04-18 18:49:47 +0530

    simplify Hibernate configuration and use persist() method

--- src/main/java/com/jason/Main.java
+++ src/main/java/com/jason/Main.java
@@ -7,23 +7,30 @@ import org.hibernate.cfg.Configuration;
 
 public class Main {
     public static void main(String args[]){
-        Student s1 = new Student();
-        s1.setsName("Kaukau");
-        s1.setRollNo(1);
-        s1.setsAge(5);
 
-        Configuration cfg = new Configuration();
-        cfg.addAnnotatedClass(com.jason.Student.class);
-        cfg.configure();
-        SessionFactory sf = cfg.buildSessionFactory();
+        Student s1 = new Student();
+        s1.setsName("Pulkit");
+        s1.setRollNo(4);
+        s1.setsAge(25);
+
+//        Configuration cfg = new Configuration();
+//        cfg.addAnnotatedClass(com.jason.Student.class);
+//        cfg.configure();
+
+        SessionFactory sf = new Configuration()
+                .addAnnotatedClass(com.jason.Student.class)
+                .configure()
+                .buildSessionFactory();
 
         Session session = sf.openSession();
 
         Transaction transaction = session.beginTransaction();
 
-        session.save(s1);
+        session.persist(s1);
 
         transaction.commit();
+        session.close();
+        sf.close();
 
         System.out.println(s1);
     }
