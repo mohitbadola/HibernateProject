31c00a494441b5debd28b2c481f415d37a43baa2
Mohit Badola <94218660+mohitbadola@users.noreply.github.com>
2025-04-18 17:57:14 +0530

    create Student class

--- src/main/java/org/jason/Main.java
+++ src/main/java/org/jason/Main.java
@@ -1,7 +1,16 @@
 package org.jason;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class Main {
     public static void main(String args[]){
-        System.out.println("Hello world");
+        Student s1 = new Student();
+        s1.setsName("Kaukau");
+        s1.setRollNo(1);
+        s1.setsAge(5);
+
+        System.out.println(s1);
     }
 }
