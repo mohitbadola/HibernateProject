905ccdcadfef3fd5a2f4a62024dd7cc6ed14104d
Mohit Badola <94218660+mohitbadola@users.noreply.github.com>
2025-04-18 19:07:17 +0530

    implement Student entity retrieval using Hibernate session.get()

--- src/main/java/com/jason/Main.java
+++ src/main/java/com/jason/Main.java
@@ -8,15 +8,34 @@ import org.hibernate.cfg.Configuration;
 public class Main {
     public static void main(String args[]){
 
-        Student s1 = new Student();
-        s1.setsName("Pulkit");
-        s1.setRollNo(4);
-        s1.setsAge(25);
+//        To save data in table
+//        Student s1 = new Student();
+//
+//        s1.setsName("Pulkit");
+//        s1.setRollNo(4);
+//        s1.setsAge(25);
+//
+//        SessionFactory sf = new Configuration()
+//                .addAnnotatedClass(com.jason.Student.class)
+//                .configure()
+//                .buildSessionFactory();
+//
+//        Session session = sf.openSession();
+//
+//        Transaction transaction = session.beginTransaction();
+//
+//        session.persist(s1);
+//
+//        transaction.commit();
+//        session.close();
+//        sf.close();
+//
+//        System.out.println(s1);
 
-//        Configuration cfg = new Configuration();
-//        cfg.addAnnotatedClass(com.jason.Student.class);
-//        cfg.configure();
 
+
+//        To get data from Table
+        Student s2 = null;
         SessionFactory sf = new Configuration()
                 .addAnnotatedClass(com.jason.Student.class)
                 .configure()
@@ -24,14 +43,10 @@ public class Main {
 
         Session session = sf.openSession();
 
-        Transaction transaction = session.beginTransaction();
-
-        session.persist(s1);
-
-        transaction.commit();
+        s2 = session.get(Student.class, 1);
         session.close();
         sf.close();
 
-        System.out.println(s1);
+        System.out.println(s2);
     }
 }
