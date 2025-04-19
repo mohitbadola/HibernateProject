ffceada66c952365cce627f586b71708425890ce
Mohit Badola <94218660+mohitbadola@users.noreply.github.com>
2025-04-19 18:59:18 +0530

    Create Engineer.java

---
+++ src/main/java/com/jason/Engineer.java
@@ -0,0 +1,60 @@
+package com.jason;
+
+//import jakarta.persistence.Embedded;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Engineer {
+
+    @Id
+    private int eid;
+    private String eName;
+    private String tech;
+
+    @Embedded
+    private Laptop laptop;
+
+    public int getEid() {
+        return eid;
+    }
+
+    public void setEid(int eid) {
+        this.eid = eid;
+    }
+
+    public String geteName() {
+        return eName;
+    }
+
+    public void seteName(String eName) {
+        this.eName = eName;
+    }
+
+    public String getTech() {
+        return tech;
+    }
+
+    public void setTech(String tech) {
+        this.tech = tech;
+    }
+
+    public Laptop getLaptop() {
+        return laptop;
+    }
+
+    public void setLaptop(Laptop laptop) {
+        this.laptop = laptop;
+    }
+
+    @Override
+    public String toString() {
+        return "Engineer{" +
+                "eid=" + eid +
+                ", eName='" + eName + '\'' +
+                ", tech='" + tech + '\'' +
+                ", laptop=" + laptop +
+                '}';
+    }
+}
